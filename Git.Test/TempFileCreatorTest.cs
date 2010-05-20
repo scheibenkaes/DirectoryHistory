27806fa88cedcb01be33831256bd28f2e1aabbaf
Benjamin <scheibenkaes@googlemail.com>
2010-05-21 00:21:35 +0200

    Added git impl for ITempFileCreator + UTs

---
+++ Git.Test/TempFileCreatorTest.cs
@@ -0,0 +1,47 @@
+//  
+//  TempFileCreatorTest.cs
+//  
+//  Author:
+//       Benjamin Klüglein <scheibenkaes@googlemail.com>
+// 
+//  Copyright (c) 2010 Benjamin Klüglein
+// 
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+using System.IO;
+using NUnit.Framework;
+
+using DirectoryHistory.History;
+using DirectoryHistory.History.Git;
+
+namespace Git.Test
+{
+	[TestFixture()]
+	public class TempFileCreatorTest
+	{
+		[Test]
+		public void Implements_ITempFileCreator ()
+		{
+			Assert.IsInstanceOfType (typeof (ITempFileCreator), new TempFileCreator ());
+		}
+		
+		[Test]
+		public void Creates_ATempFile ()
+		{
+			var testFile = "";
+			Assert.IsTrue (File.Exists (testFile));
+		}
+	}
+}
+
