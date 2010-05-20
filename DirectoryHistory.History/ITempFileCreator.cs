df77f50c229e378bee7b1ed13598dc09ba9d491f
Benjamin <scheibenkaes@googlemail.com>
2010-05-21 00:11:50 +0200

    Added interface ITempFileCreator

---
+++ DirectoryHistory.History/ITempFileCreator.cs
@@ -0,0 +1,42 @@
+//  
+//  ITempFileCreator.cs
+//  
+//  Author:
+//       Benjamin Klüglein <scheibenkaes@googlemail.com>
+// 
+//  Copyright (c) 2010 Benjamin Klüglein
+// 
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+
+namespace DirectoryHistory.History
+{
+	public interface ITempFileCreator
+	{
+		/// <summary>
+		/// Create a temporary file from the version of a file
+		/// </summary>
+		/// <param name="file">
+		/// Must not be null
+		/// </param>
+		/// <param name="version">
+		/// Must not be null
+		/// </param>
+		/// <returns>
+		/// Path to the newly created file
+		/// </returns>
+		string CreateTempFileFromVersion (IFileWithHistory file, IFileVersion version);
+	}
+}
+
